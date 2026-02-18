848982c77c03c15fe62c48729a164c06ebfb6534
Mohammad Fathian <167574608+mmdfathian@users.noreply.github.com>
2026-02-19 02:15:12 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+"""Module for loading environment configurations."""
 import os
 from dotenv import load_dotenv
 
@@ -7,5 +8,4 @@ API_ID = os.getenv('API_ID')
 API_HASH = os.getenv('API_HASH')
 
 if not API_ID or not API_HASH:
-    # Critical error if .env is missing or empty
     raise ValueError("ERROR: API_ID or API_HASH not found in .env file!")
