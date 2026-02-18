6fc5415e669c4cd94cdd96c6f364331aafc9b036
Mohammad Fathian <167574608+mmdfathian@users.noreply.github.com>
2026-02-19 00:50:05 +0330

    Create logger_utils.py

---
+++ logger_utils.py
@@ -0,0 +1,14 @@
+import logging
+from datetime import datetime
+
+def get_logger(name):
+    log_filename = f"broadcast_{datetime.now().strftime('%Y%m%d')}.log"
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.FileHandler(log_filename, encoding='utf-8'),
+            logging.StreamHandler()
+        ]
+    )
+    return logging.getLogger(name)
